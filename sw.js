733e614d1a34f04fdce7589471c33ac4094a8615
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-12 17:49:43 +0200

    workbox

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+importScripts(
+  "./node_modules/workbox-sw/build/importScripts/workbox-sw.dev.v2.1.1.js"
+);
+
 const staticAssets = [
   "./",
   "./styles.css",
@@ -6,38 +10,20 @@ const staticAssets = [
   "./images/fetch-cat.jpg"
 ];
 
-self.addEventListener("install", async e => {
-  const cache = await caches.open("news-static");
-  cache.addAll(staticAssets);
-});
+const wb = new WorkboxSW();
 
-self.addEventListener("fetch", e => {
-  const req = e.request;
+wb.precache(staticAssets);
 
-  const url = new URL(req.url);
+wb.router.registerRoute(
+  "https://newsapi.org/(.*)",
+  wb.strategies.networkFirst()
+);
 
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkFirst(req));
-  }
-});
-
-async function cacheFirst(req) {
-  const cachedResponse = await caches.match(req);
-  return cachedResponse || fetch(req);
-}
-
-async function networkFirst(req) {
-  const cache = await caches.open("news-dynamic");
-
-  try {
-    const res = await fetch(req);
-    cache.put(req, res.clone());
-    return res;
-  } catch (e) {
-    const cachedResponse = await cache.match(req);
-
-    return cachedResponse || (await caches.match("./fallback.json"));
-  }
-}
+wb.router.registerRoute(
+  /.*\.(png|jpg|jpeg|gif)/,
+  wb.strategies.cacheFirst({
+    cacheName: "news-image",
+    cacheExpiration: { maxEntries: 20, maxAgeSeconds: 12 * 60 * 60 },
+    cacheableResponse: { statuses: [0, 200] }
+  })
+);
